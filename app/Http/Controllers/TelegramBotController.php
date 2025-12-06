77aa86c7af606bebb4ec79dc9abbac4bbb531831
undefined <zuenko@gmail.com>
2025-12-06 23:13:08 +0700

    init

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -6,6 +6,7 @@ use App\Models\Slot;
 use App\Models\TelegramState;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Http;
 use Carbon\Carbon;
 
