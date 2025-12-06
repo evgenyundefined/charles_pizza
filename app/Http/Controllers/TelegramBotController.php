a829a7c6521ffbe03edf0f4b9abecbea3e6012b0
undefined <zuenko@gmail.com>
2025-12-07 00:57:44 +0700

    init

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -1126,7 +1126,7 @@ class TelegramBotController extends Controller
             } else {
                 // не выполнен — добавляем кнопку
                 $keyboard['inline_keyboard'][] = [[
-                    'text' => "Пометить выполненым заказ {$time} для {$username} ✅",
+                    'text' => "Выполнен {$time} {$username} ✅",
                     'callback_data' => 'done:' . $slot->id,
                 ]];
             }
